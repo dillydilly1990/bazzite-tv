027d573c6072cbd8bdfd7b1e4fd7a6337c23804f
dillydilly1990 <69658010+dillydilly1990@users.noreply.github.com>
2025-02-20 09:41:49 -0500

    update files/scripts/flexlauncher.sh

---
+++ files/scripts/flexlauncher.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# Tell this script to exit if there are any errors.
+# You should have this in every custom script, to ensure that your completed
+# builds actually ran successfully without any errors!
+set -oue pipefail
+
+# Your code goes here.
+echo 'install build dependencys'
+sudo dnf install SDL2-devel SDL2_image-devel SDL2_ttf-devel inih-devel
+echo 'clone master repo and create build dir'
+git clone https://github.com/complexlogic/flex-launcher.git
+cd flex-launcher
+mkdir build && cd build
+echo 'generate make file'
+cmake .. -DCMAKE_BUILD_TYPE=Release
+echo 'build and test'
+make
+./flex-launcher
+echo 'install'
+sudo make install
