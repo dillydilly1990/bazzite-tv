ffb6f3f68fa1343108b4147e21d5f229679c8dee
dillydilly1990 <69658010+dillydilly1990@users.noreply.github.com>
2025-03-29 10:44:41 -0400

    update files/scripts/quickemu.sh

---
+++ files/scripts/quickemu.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -ouex pipefail
+
+# it may be needed to add additional installation sources to obtain some packages
+# for example zsync is no longer in the fedora repo, but can be obtained from copr
+dnf copr enable elxreno/zsync -y 
+rpm-ostree install bash coreutils curl edk2-tools genisoimage grep jq mesa-demos pciutils procps python3 qemu sed socat spice-gtk-tools swtpm unzip usbutils util-linux xdg-user-dirs xrandr zsync
+git clone https://github.com/quickemu-project/quickemu
+cd quickemu
+make install quickemu
